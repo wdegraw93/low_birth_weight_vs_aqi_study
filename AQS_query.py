f87571b6b817e1989871c3d8cb0ec272dae0b2a8
wdegraw93 <winston.degraw@gmail.com>
2023-01-16 23:13:06 -0800

    AQS_query now combines params into single query

--- AQS_query.py
+++ AQS_query.py
@@ -101,23 +101,31 @@ beginning_year = int(input('First year of interest: '))
 end_year = int(input('Last year of interest (if only want one year put the same year): '))
 
 state_codes = get_state_codes(email,key)
-param_codes = get_param_codes_by_class(email, key, 'CRITERIA')
+param_codes = get_param_codes_by_class(email, key, 'CRITERIA').astype(str)
+params_1 = ','.join(param_codes[:4].values)
+params_2 = ','.join(param_codes[4:].values)
+params = [params_1, params_2]
 years = np.arange(beginning_year, end_year+1).astype(str)
 
 data_list = []
+state_count=0
 for state_code, state in state_codes.values[:-2]:
-    # Query for county codes at the top to avoid repeated queries
+    state_count += 1
+# Query for county codes at the top to avoid repeated queries
     county_codes = get_county_codes(email,key,state_code)
     for year in years:
         print('--------------------')
-        print(f'Collecting data for {state} in {year}')
+        print(f'Collecting data for state {state} in year {year} ({state_count}/{len(state_codes[:-2])})')
         print('--------------------')
-        for param in param_codes:
-            for county in county_codes:
+        county_count = 0
+        for county in county_codes:
+            county_count += 1
+            print(f'County {county_count}/{len(county_codes)}')
+            for param in params:
                 data_list.append( aqs_api_annual_county(email, key, param, year+'0101', year+'1231', state_code, county) )
                 
 data = pd.concat(data_list, ignore_index=True)
 if beginning_year == end_year:
-    data.to_csv(f'data/AQS_county_data_{beginning_year}.csv')
+    data.to_csv(f'data/AQS_county_data_{beginning_year}.csv', index=False)
 else:
-    data.to_csv(f'data/AQS_county_data_{beginning_year}_{end_year}.csv')
+    data.to_csv(f'data/AQS_county_data_{beginning_year}_{end_year}.csv', index=False)
